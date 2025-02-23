bfcb55653768bd55e91736374f03cc79b8a56ac6
Rodrigo Pariente <rodrigomanedio17@gmail.com>
2025-02-22 21:20:09 -0400

    Actions refactored.

--- actions.py
+++ actions.py
@@ -1,46 +1,45 @@
 from collections.abc import Callable
-from data_navigator import DataNavigator
 from path_utils import path_pop
 from file_utils import save_json
 import sys
 
+
+commands = {}
+
+def add_command(command: str) -> Callable:
+    def wrapper(func):
+        commands[command] = func
+        return func
+    return wrapper
+
+@add_command("\\exit")
 def exit(*args) -> None:
     sys.exit(0) # wanted to be break
 
-def restart(dn: DataNavigator) -> None:
+@add_command("\\restart")
+def restart(dn) -> None:
     dn.data = dn.secure_data
 
-def save(dn: DataNavigator) -> None:
+@add_command("\\save")
+def save(dn) -> None:
     save_json(dn.filename, dn.data)
     print(f"Saved at {dn.filename}.")
 
-def switch_literal(flag: str) -> Callable:
-    def func(dn: DataNavigator):
-        if flag == "on":
-            dn.literal = True
-        else:
-            dn.literal = False
+@add_command("\\+l")
+def literal_on(dn) -> None:
+    dn.literal = True
 
-        print(f"Literal flag turned {flag}.")
-    return func
+@add_command("\\-l")
+def literal_off(dn) -> None:
+    dn.literal = False
 
+@add_command("\\")
 def do_nothing(*args) -> None:
     pass
 
-def back(dn: DataNavigator) -> None:
+@add_command("..")
+def back(dn) -> None:
     if dn.path:
         dn.path = path_pop(dn.path)
     else:
         print("ERROR: You are at root level.")
-
-
-actions = {
-    "\\exit": exit,
-    "\\restart": restart,
-    "\\save": save,
-    "\\-l": switch_literal("off"),
-    "\\+l": switch_literal("on"),
-    "\\": do_nothing,
-    "..": back,
-}
-
